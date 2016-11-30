6b7aa73ec55baf205d743312b0155a6534ebe03b
srose1 <srose@fh-bielefeld.de>
2016-11-30 12:50:59 +0100

    changed url to https://tinytaskrest.herokuapp.com not functioning

--- TinyTask/src/restService/restService.ts
+++ TinyTask/src/restService/restService.ts
@@ -24,91 +24,91 @@ export class RestService {
 
 // Einen einzelnen Task holen
   getTask(task_id) {
-    var url = 'http://localhost/tasks/?query=&query=' + encodeURI(task_id) + '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    var url = ' https://tinytaskrest.herokuapp.com/tasks/' + encodeURI(task_id);
     var response = this.http.get(url).map(res => res.json());
     return response;
   }
 
   //Alle tasks holen
   getAllTasks(){
-    var url = 'http://localhost/tasks' + '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    var url = ' https://tinytaskrest.herokuapp.com/tasks/';
     var response = this.http.get(url).map(res => res.json());
     return response;
   }
 
   // Längen und  Breitengrad eines Tasks holen
   getTaskPosition(task_id){
-    var url = 'http://localhost/tasks/?query=&query=' + encodeURI(task_id) + '/position' + '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    var url = ' https://tinytaskrest.herokuapp.com/tasks/' + encodeURI(task_id) + '/position';
     var response = this.http.get(url).map(res => res.json());
     return response;
   }
 
   // Alle Bewerber eines Tasks hole
   getTaskBewerber(task_id){
-    var url = 'http://localhost/tasks/?query=&query=' + encodeURI(task_id) + '/applications' + '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    var url = ' https://tinytaskrest.herokuapp.com/tasks/' + encodeURI(task_id) + '/applications';
     var response = this.http.get(url).map(res => res.json());
     return response;
   }
 
   //Eingegrenzten Tasks holen
-  getTaskOptional(radius , startzeit){
-    var url = 'http://localhost:8080/tasks?query=&query=' + encodeURI(radius) +'&' +'&query=&query=' + encodeURI(startzeit)+'&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
-    var response = this.http.get(url).map(res => res.json());
-    return response;
-  }
+  //getTaskOptional(radius , startzeit){
+    //var url = ' https://tinytaskrest.herokuapp.com/tasks?query=&query=' + encodeURI(radius) +'&' +'&query=&query=' + encodeURI(startzeit)+'&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    //var response = this.http.get(url).map(res => res.json());
+    //return response;
+  //}
 
   //Liste aller Benutzer holen
   getUserAll(){
-    var url = 'http://localhost:8080/users' + '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    var url = ' https://tinytaskrest.herokuapp.com/users';
     var response = this.http.get(url).map(res => res.json());
     return response;
   }
 
   //Eine Nutzer Bewertung holen
   getUserRating(user_id) {
-    var url = 'http://localhost:8080/users/?query=&query=' + encodeURI(user_id)+'/rating'+'&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    var url = ' https://tinytaskrest.herokuapp.com/users/' + encodeURI(user_id)+'/rating';
     var response = this.http.get(url).map(res => res.json());
     return response;
   }
 
   //Gibt bestimmten Benutzer wieder
   getUserSingle(user_id){
-    var url = 'http://localhost:8080/users/?query=&query=' + encodeURI(user_id) +'&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    var url = ' https://tinytaskrest.herokuapp.com/users/' + encodeURI(user_id);
     var response = this.http.get(url).map(res => res.json());
     return response;
   }
 
   // Neuen Task erstellen , wahrscheinlich überarbeiten
   newTask(newTask){
-    var url = 'http://localhost:8080/tasks' + '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    var url = ' https://tinytaskrest.herokuapp.com/tasks/';
     var response = this.http.post(url,newTask,null);
     return response;
   }
 
   //Task löschen
   deleteTask(task_id){
-    var url = 'http://localhost:8080/tasks/?query=&query=' + encodeURI(task_id) + '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    var url = ' https://tinytaskrest.herokuapp.com/tasks/' + encodeURI(task_id);
     var response =this.http.delete(url);
     return response;
   }
 
   //Benutzer hinzufügen
   newUser(newUser){
-    var url = 'http://localhost:8080/users' + '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    var url = ' https://tinytaskrest.herokuapp.com/users/';
     var response = this.http.post(url,newUser,null);
     return response;
   }
 
   //Bewertung abgeben
   newUserRating(user_id) {
-    var url = 'http://localhost:8080/users/?query=&query=' + encodeURI(user_id) + '/rating' + '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    var url = ' https://tinytaskrest.herokuapp.com/users/' + encodeURI(user_id) + '/rating';
     var response = this.http.post(url,user_id,null);
     return response;
   }
 
   //User löschen
   deleteUser(user_id){
-    var url = 'http://localhost/users/?query=&query=' + encodeURI(user_id) +'&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    var url = ' https://tinytaskrest.herokuapp.com/users/' + encodeURI(user_id);
     var response = this.http.delete(url);
     return response;
 
