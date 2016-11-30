29014405564ffd22e56041631657adc6669c9ae2
srose1 <srose@fh-bielefeld.de>
2016-11-30 15:14:23 +0100

    function newTask //not functioning

--- TinyTask/src/pages/newtask/newtask.ts
+++ TinyTask/src/pages/newtask/newtask.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import {Http, Headers} from 'angular2/http';
+import {AuthHttp, tokenNotExpired} from 'angular2-jwt';
 import { NavController } from 'ionic-angular';
+import {AuthService} from '../../services/auth/auth.service';
+import 'rxjs/add/operator/map';
 
 /*
   Generated class for the Newtask page.
@@ -12,8 +16,37 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'newtask.html'
 })
 export class NewtaskPage {
+  API: string = "https://tinytaskrest.herokuapp.com/tasks/";
 
-  constructor(public navCtrl: NavController) {}
+  error: string;
+  auth: AuthService;
+  constructor(
+    public navCtrl: NavController,
+    //private http: Http,
+    private authHttp: AuthHttp,
+    private headers = new Headers(
+      {'Content-Type': 'application/json'}),
+    private name: string,
+    private description: string,
+    private payment: number,
+    private latitude: number,
+    private longitude: number,
+    private starts: string,
+    private category: string
+    ){
+
+  }
+  //so ähnlich
+  postNewTask() {
+    return this.authHttp.post(`${this.API}`, JSON.stringify({
+      name: this.name,
+      description: this.description,
+      payment: this.payment,
+      position: {latitude: this.latitude, longitude: this.longitude},
+      starts: this.starts,
+      category: this.category
+    }), {headers: this.headers}).map(res => res.text()).subscribe(err => this.error = err);
+  }
 
   ionViewDidLoad() {
     console.log('Hello NewtaskPage Page');
