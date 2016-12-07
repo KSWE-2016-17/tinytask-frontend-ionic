3cc8219bfd3b6263cbae29a6281bd7e30d38765d
srose1 <srose@fh-bielefeld.de>
2016-12-07 14:01:44 +0100

    buggy

--- TinyTask/src/pages/newtask/newtask.ts
+++ TinyTask/src/pages/newtask/newtask.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Http, Headers} from 'angular2/http';
+import {Http, Headers} from '@angular/http';
 import {AuthHttp, tokenNotExpired} from 'angular2-jwt';
 import { NavController } from 'ionic-angular';
 import {AuthService} from '../../services/auth/auth.service';
@@ -24,8 +24,6 @@ export class NewtaskPage {
     public navCtrl: NavController,
     //private http: Http,
     private authHttp: AuthHttp,
-    private headers = new Headers(
-      {'Content-Type': 'application/json'}),
     private name: string,
     private description: string,
     private payment: number,
@@ -45,7 +43,7 @@ export class NewtaskPage {
       position: {latitude: this.latitude, longitude: this.longitude},
       starts: this.starts,
       category: this.category
-    }), {headers: this.headers}).map(res => res.text()).subscribe(err => this.error = err);
+    })).map(res => res.text()).subscribe(err => this.error = err);
   }
 
   ionViewDidLoad() {
