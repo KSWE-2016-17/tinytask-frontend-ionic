dc91aaf6b8ea67384a16a0b72f25ca813dd143ba
csassmann <christian_philipp.sassmann@fh-bielefeld.de>
2016-12-07 14:08:16 +0100

    Auth0 Variablen an REST Vorgabe angepasst

--- TinyTask/src/auth0-variables.ts
+++ TinyTask/src/auth0-variables.ts
@@ -1,5 +1,5 @@
 export class Auth0Vars {
-    static AUTH0_CLIENT_ID = 'toyZGa09AT3I2XSwI6Do2KiEcubH6dwH';
-    static AUTH0_DOMAIN = 'csassmann.eu.auth0.com';
+    static AUTH0_CLIENT_ID = 'hVGlnq1D4g8-y7UmCkPaWN33FwE11TOmq2_-3J_4FJF6Qk9n1NMwQ_Z2LPEM4-KR';
+    static AUTH0_DOMAIN = 'tinytask.eu.auth0.com';
     static AUTH0_CALLBACK_URL = location.href;
 }
